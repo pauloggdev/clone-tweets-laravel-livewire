e87608488ee39a3bb6af85c0db26274d1a766d4e
pauloggdev <pauloggjoao@gmail.com>
2022-06-29 13:44:24 +0100

    first commit

---
+++ app/Http/Livewire/ShowTweets.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Tweet;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class ShowTweets extends Component
+{
+    use WithPagination;
+
+    
+    public $content = "Apenas uma mensagem";
+    protected $rules = [
+        'content' => 'required|min:3|max:255'
+    ];
+
+
+    public function render()
+    {
+        $data['tweets'] = Tweet::with('user')->paginate(2);
+        return view('livewire.show-tweets', $data);
+    }
+
+    public function create()
+    {
+        $this->validate();
+        Tweet::create([
+            'content' => $this->content,
+            'user_id' => 1
+        ]);
+
+        $this->content = "";
+    }
+}
